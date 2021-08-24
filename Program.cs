e0af959645d255de49fd509618c428a8cf1df564
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-08-24 12:38:39 -0300

    consulta com Dapper

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.Data.SqlClient;
+using Dapper;
+using BaltaDataAcess.Models;
 
 namespace BaltaDataAcess
 {
@@ -11,19 +13,9 @@ namespace BaltaDataAcess
 
             using (var connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Conectado");
-                connection.Open();
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-                    var reader = command.ExecuteReader();
-
-                    while(reader.Read()){
-                        Console.WriteLine($"{reader.GetGuid(0)} = {reader.GetString(1)}");
-                    }
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach(var category in categories){
+                    Console.WriteLine($"{category.Id} - {category.Title}");
                 }
             }
         }
