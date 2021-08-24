25cee321554bf7bfb6f91c0345eaead62a18892c
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-08-24 17:03:57 -0300

    insert with Dapper

--- Program.cs
+++ Program.cs
@@ -11,11 +11,41 @@ namespace BaltaDataAcess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+            var insertSql = @"INSERT INTO 
+                                [Category] 
+                            VALUES(
+                                @Id, 
+                                @Title, 
+                                @Url, 
+                                @Summary, 
+                                @Order, 
+                                @Description, 
+                                @Featured)";
             using (var connection = new SqlConnection(connectionString))
             {
+                var rows = connection.Execute(insertSql, new {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+
+                Console.WriteLine($"{rows} linhas inseridas");
                 var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach(var category in categories){
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
         }
